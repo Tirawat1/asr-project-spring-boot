c5dbf241522a751fcfd2721e0ac8b7d0335cbdb8
tirawat1 <tirawat.pst@gmail.com>
2025-02-18 00:07:16 +0700

    init commit

---
+++ src/test/java/cs/project/TextToSpeech/TextToSpeechApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.project.TextToSpeech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSpeechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
