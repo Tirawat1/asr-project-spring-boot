30a1dbb80f10844e3ddd67ba2168f357009bc95e
tirawat1 <tirawat.pst@gmail.com>
2025-03-03 16:43:16 +0700

    update a  exist id when create user

--- src/main/java/cs/project/TextToSpeech/infra/repository/UserRepository.java
+++ src/main/java/cs/project/TextToSpeech/infra/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package cs.project.TextToSpeech.infra.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -8,4 +9,6 @@ import cs.project.TextToSpeech.models.UserModel;
 
 public interface UserRepository extends MongoRepository<UserModel, String> {
     List<UserModel> findByWorkspaceIdsContaining(String workspaceId); 
+    Optional<UserModel> findByEmail(String email);
+    Optional<UserModel> findByName(String name);
 }
